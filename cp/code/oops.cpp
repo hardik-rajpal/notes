b18055e749302cbe66b104cc6cac16da5710d273
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-09-26 22:48:53 +0530

    somewhere.

--- cp/code/oops.cpp
+++ cp/code/oops.cpp
@@ -1,41 +1,36 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-class Shape{
-    public:
-    int width,height;
-    virtual int area(){
-        cout<<"Shape area"<<endl;
-        return 0;
-    };
+
+class Base
+{
+protected:
+	int a;
+public:
+	Base() {a = 0;}
 };
-class Rectangle:public Shape{
-    public:
-    int area(){
-        cout<<"Rect Area"<<endl;
-        return 0;
-    }
+
+class Derived1: public Base
+{
+public:
+	int c;
 };
-class Square:public Rectangle{
-    int area(){
-        cout<<"Square Area"<<endl;
-        return 0;
-    }
+
+
+class Derived2: public Base
+{
+public:
+	int c;
 };
-class Triangle:public Shape{
-    public:
-    int area(){
-        cout<<"Triangle Area"<<endl;
-        return 0;
-    }
+
+class DerivedDerived: public Derived1, public Derived2
+{
+public:
+	void show() { cout << Base::a; }
 };
-void printarea(Shape s){
-    s.area();
-}
-int main(){
-    Shape *s = new Square;
-    Rectangle *r = new Square;
-    printarea(*s);
-    Shape &s2 = *r;
-    s2.area();
-    cout<<"Exiting gracefully!"<<endl;
+
+int main(void)
+{
+	DerivedDerived d;
+	d.show();
+	return 0;
 }
