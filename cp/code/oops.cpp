4823e251f21d6f604402cd7bcbac52e784f9dc10
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-10-06 11:56:12 +0530

    python notes

--- cp/code/oops.cpp
+++ cp/code/oops.cpp
@@ -18,9 +18,12 @@ class Display: public Base{
     }
     Display(const Display&d) = delete;
 };
+int safemod(int a, int m){
+    return (m + (a % m)) % m;
+}
 int main()
 {   
-    Display d;
-    throw d;
+    int a = -7;
+    cout<<(a/2)<<endl;
     return 0;
 }
